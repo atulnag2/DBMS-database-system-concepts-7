06614939def1e4ecd1bf8bb6b10481aa63b1b46b
Atul Nag <130294960+atulnag2@users.noreply.github.com>
2023-12-08 21:52:57 +0530

    Update Chapter3.sql

--- Chapter3.sql
+++ Chapter3.sql
@@ -119,47 +119,135 @@
 -- 3.12 Write the SQL statements using the university schema to perform the following operations:
 
 -- a. Create a new course “CS-001”, titled “Weekly Seminar”, with 0 credits.
-
+INSERT INTO course (course_id, title,dept_name, credits) 
+VALUES ('CS-001','Weekly Seminar','Comp. Sci.', 0);
 -- b. Create a section of this course in Fall 2017, with sec id of 1, and with the location of this section not yet specified.
-
+INSERT INTO section (course_id, sec_id, semester, year) VALUES 
+('CS-001', '1', 'Fall', 2017)
 -- c. Enroll every student in the Comp. Sci. department in the above section.
-
+INSERT INTO takes (id, course_id, sec_id, semester, year) 
+SELECT student.id,'CS-001', '1', 'Fall', 2017
+FROM student 
+WHERE student.dept_name = 'Comp. Sci.'
 -- d. Delete enrollments in the above section where the student’s ID is 12345.
-
+DELETE FROM takes 
+WHERE ID = '12345' AND (course_id, sec_id, semester, year) = ('CS-001', '1', 'Fall', 2017)
 -- e. Delete the course CS-001. What will happen if you run this delete statement without first deleting offerings (sections) of this course?
-
+DELETE FROM course
+WHERE course_id = 'CS-001'; 
 -- f. Delete all takes tuples corresponding to any section of any course with the word “advanced” as a part of the title; ignore case when matching the word with the title.
-
+DELETE FROM takes
+WHERE course_id IN (
+    SELECT course_id
+    FROM course
+    WHERE LOWER(title) LIKE '%advanced%'
+)
 
 -- 3.24 Using the university schema, write an SQL query to find the name and ID of those Accounting students advised by an instructor in the Physics department.
-
+SELECT s.id, s.name
+FROM student AS s INNER JOIN advisor AS a ON s.id = a.s_id
+                  INNER JOIN instructor AS i ON a.i_id = i.id
+WHERE s.dept_name = 'Accounting' AND i.dept_name = 'Physics'; 
 
 -- 3.25 Using the university schema, write an SQL query to find the names of those departments whose budget is higher than that of Philosophy. List them in alphabetic order
-
+SELECT dept_name
+FROM department
+WHERE budget > (SELECT budget FROM department WHERE dept_name = 'Philosophy')
+ORDER BY dept_name ASC;
 -- 3.26 Using the university schema, use SQL to do the following: For each student who has retaken a course at least twice (i.e., the student has taken the course at least three times), show the course ID and the student’s ID.
 -- Please display your results in order of course ID and do not display duplicate rows.
-
+SELECT id,course_id 
+FROM takes
+GROUP BY id,course_id
+HAVING COUNT(*) >= 3
+ORDER BY course_id ASC; 
 
 -- 3.27 Using the university schema, write an SQL query to find the IDs of those students who have retaken at least three distinct courses at least once (i.e, the student has taken the course at least two times).
-
+WITH retakers(id,course_id,frequency) AS (
+    SELECT id,course_id,COUNT(*) 
+    FROM takes
+    GROUP BY id,course_id
+    HAVING COUNT(*) > 1
+)
+SELECT id
+FROM retakers
+GROUP BY id
+HAVING COUNT(*) >= 3;
 
 -- 3.28 Using the university schema, write an SQL query to find the names and IDs of those instructors who teach every course taught in his or her department (i.e., every course that appears in the course relation with the instructor’s department name). Order result by name.
-
+SELECT id, name
+FROM instructor AS i
+WHERE NOT EXISTS (
+    (SELECT course_id FROM course WHERE dept_name = i.dept_name)
+    EXCEPT 
+    (SELECT course_id FROM teaches WHERE teaches.id = i.id)
+)
+ORDER BY name ASC
 
 -- 3.29 Using the university schema, write an SQL query to find the name and ID of each History student whose name begins with the letter ‘D’ and who has not taken at least five Music courses.
-
+SELECT id,name
+FROM student AS s
+WHERE dept_name = 'History' 
+    AND name LIKE 'D%'
+    AND (
+        SELECT COUNT(DISTINCT course_id)
+        FROM takes
+        WHERE takes.id = s.id AND 
+            course_id IN (SELECT course_id FROM course WHERE dept_name = 'Music')
+    ) < 5;
 
 -- 3.31 Using the university schema, write an SQL query to find the ID and name of each instructor who has never given an A grade in any course she or he has taught. (Instructors who have never taught a course trivially satisfy this condition.)
-
+SELECT id, name 
+FROM instructor AS i
+WHERE 'A' NOT IN (
+    SELECT takes.grade
+    FROM takes INNER JOIN teaches 
+        ON (takes.course_id,takes.sec_id,takes.semester,takes.year) = 
+           (teaches.course_id,teaches.sec_id,teaches.semester,teaches.year)
+    WHERE teaches.id = i.id
+)
 
 -- 3.32 Rewrite the preceding query, but also ensure that you include only instructors who have given at least one other non-null grade in some course.
-
+SELECT id, name 
+FROM instructor AS i
+WHERE 'A' NOT IN (
+    SELECT takes.grade
+    FROM takes INNER JOIN teaches 
+        ON (takes.course_id,takes.sec_id,takes.semester,takes.year) = 
+           (teaches.course_id,teaches.sec_id,teaches.semester,teaches.year)
+    WHERE teaches.id = i.id
+) 
+AND
+(
+    SELECT COUNT(*)
+    FROM takes INNER JOIN teaches 
+        ON (takes.course_id,takes.sec_id,takes.semester,takes.year) = 
+           (teaches.course_id,teaches.sec_id,teaches.semester,teaches.year)
+    WHERE teaches.id = i.id AND takes.grade IS NOT NULL    
+) >= 1
 -- 3.33 Using the university schema, write an SQL query to find the ID and title of each course in Comp. Sci. that has had at least one section with afternoon hours (i.e., ends at or after 12:00). (You should eliminate duplicates if any.)
-
+SELECT course_id,title
+FROM course AS c
+WHERE dept_name = 'Comp. Sci.' AND 
+    EXISTS (
+        SELECT * 
+        FROM section
+        WHERE section.course_id = c.course_id AND 
+        time_slot_id IN (SELECT time_slot_id FROM time_slot WHERE end_hr >= 12)
+    )
 
 -- 3.34 Using the university schema, write an SQL query to find the number of students in each section. The result columns should appear in the order “courseid, secid, year, semester, num”. You do not need to output sections with 0 students.
-
+SELECT course_id, sec_id, year, semester, COUNT(DISTINCT ID) AS num
+FROM takes
+GROUP BY course_id, sec_id, year, semester;
 
 -- 3.35 Using the university schema, write an SQL query to find section(s) with maximum enrollment. The result columns should appear in the order “courseid, secid, year, semester, num”. (It may be convenient to use the with construct.)
-
+WITH section_student_frequency(courseid, secid, year, semester, num) AS (
+    SELECT course_id, sec_id, year, semester, COUNT(DISTINCT ID)
+    FROM takes
+    GROUP BY course_id, sec_id, year, semester
+)
+SELECT *
+FROM section_student_frequency
+WHERE num = (SELECT MAX(num) FROM section_student_frequency);
 
